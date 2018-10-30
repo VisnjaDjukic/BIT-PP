3b7950e5fa68d68ee674d5af397cad71f174391b
BIT student <classroom@bgit.rs>
2018-10-30 16:40:50 +0100

    V-week-2day

--- V WEEK/1_day/airport.js
+++ V WEEK/1_day/airport.js
@@ -15,9 +15,17 @@
         this.number = number;
         this.category = category;
 
-        if (typeof number === 'undefined' || number === '') {
 
+        if (typeof number === 'undefined' || number === '') {
             this.number = Math.floor((Math.random() * 90) + 10);
+        }
+        else if (arguments[0] === 'e') {
+            this.number = Math.floor((Math.random() * 90) + 10);
+            this.category = 'e';
+        }
+        else if (arguments[0] === 'b') {
+            this.number = Math.floor((Math.random() * 90) + 10);
+            this.category = 'b';
         }
         if (typeof category === 'undefined') {
 
@@ -34,39 +42,86 @@
         this.seat = seat;
 
         this.getData = function () {
-            return seat.getData() + ', ' + person.getData();
+            return '\t' + seat.getData() + ', ' + person.getData();
         };
     }
 
     function Flight(relation, date) {
 
         this.relation = relation;
-        this.date = date.toLocaleDateString();
+        this.date = date;
         this.listOfPassengers = [];
+
+        this.newAbbForRelation = function () {
+
+            var splitDestination = this.relation.split(' - ');
+            //console.log(splitDestination);
+            var firstAbb = lastConsonant(splitDestination[0]);
+            var secondAbb = lastConsonant(splitDestination[1]);
+
+            function lastConsonant(string) {
+                var i;
+                var temp;
+                for (i = string.length - 1; i >= 0; i--) {
+
+                    if (string[i].toLowerCase() != 'a' && string[i] != 'e' && string[i] != 'i' && string[i] != 'o' && string[i] != 'u') {
+                        temp = string[i];
+                        break;
+                    }
+                }
+                return string[0] + temp.toUpperCase();
+            }
+
+            return firstAbb + ' - ' + secondAbb;
+        }
 
         this.addPassenger = function (newPassenger) {
             this.listOfPassengers.push(newPassenger);
         };
+
         this.getData = function () {
 
-            var final = '\t' + this.listOfPassengers[0].getData() + '\n';
-            for (var i = 1; i < this.listOfPassengers.length; i++) {
+            var final = '';
 
+            for (var i = 0; i < this.listOfPassengers.length; i++) {
 
                 final += '\t' + this.listOfPassengers[i].getData() + '\n';
+
             }
 
-            return this.date + ', ' + this.relation + '\n' + final;
+            return this.date.toLocaleDateString() + ', ' + this.newAbbForRelation() + '\n' + final;
         }
     }
+
 
     function Airport(name) {
         this.name = name;
         this.listOfFlights = [];
+        this.totalNumberOfPassengers = 0;
 
         this.addFlight = function (newFlight) {
             this.listOfFlights.push(newFlight);
         };
+
+        this.getData = function () {
+
+            var final = '';
+
+            for (var i = 0; i < this.listOfFlights.length; i++) {
+
+                final += '\t' + this.listOfFlights[i].getData() + '\n';
+
+                this.totalNumberOfPassengers += this.listOfFlights[i].listOfPassengers.length;
+            }
+
+            return 'Airport: ' + this.name + ', ' + 'total passengers: ' + this.totalNumberOfPassengers + '\n' + final;
+        };
+    }
+
+    function createFlight(relation, date) {
+
+        var randomFlight = new Fight(realtion, date);
+        return randomFlight;
     }
 
     function createPassenger(name, surname, seatNumber, category) {
@@ -75,36 +130,61 @@
         var randomSeat = new Seat(seatNumber, category);
         var randomPassenger = new Passenger(randomPerson, randomSeat);
 
-        return randomPassenger.getData();
+        return randomPassenger;
 
     }
 
-    var tri = createPassenger('visnja', 'Mrdelj', '', 'b');
-    console.log(tri)
+    var flight1 = new Flight('Belgrade - New York', new Date('2017-10-25'));
+    var flight2 = new Flight('Barcelona - Belgrade', new Date('2017-11-11'));
 
+    var passenger1 = createPassenger('John', 'Snow', 1, 'b');
+    var passenger2 = createPassenger('Cersei', 'Lanister', 2, 'b');
+    var passenger3 = createPassenger('Daenerys', 'Targaryen', 14);
+    var passenger4 = createPassenger('Tyrion', 'Lannister');
 
-
-    var person1 = new Person('Dusica', 'Savic');
-    console.log(person1.getData());
-
-    var person2 = new Person('Visnja', 'Djukic');
-    var seat2 = new Seat();
-
-    var seat1 = new Seat('', 'b');
-    console.log(seat1.getData());
-
-    var passenger1 = new Passenger(person1, seat1);
-    var passenger2 = new Passenger(person2, seat2);
-
-    console.log(passenger1.getData());
-    console.log(passenger2.getData());
-
-    var flight1 = new Flight("Belgrade-Paris", new Date("2018-12-31"));
     flight1.addPassenger(passenger1);
     flight1.addPassenger(passenger2);
+    flight2.addPassenger(passenger3);
+    flight2.addPassenger(passenger4);
+
+
+    var airport1 = new Airport('Nikola Tesla');
+    airport1.addFlight(flight1);
+    airport1.addFlight(flight2);
+
+    console.log(airport1.getData());
+
+    // var person1 = new Person('Dusica', 'Savic');
+    // console.log(person1.getData());
+
+    // var person2 = new Person('Visnja', 'Djukic');
+    // var seat2 = new Seat();
+
+    // var seat1 = new Seat('', 'b');
+    // console.log(seat1.getData());
+
+    // var seat2 = new Seat();
+    // console.log(seat2.getData());
+
+    // var passenger1 = new Passenger(person1, seat1);
+    // var passenger2 = new Passenger(person2, seat2);
+
+    // console.log(passenger1.getData());
+    // console.log(passenger2.getData());
+
+    // var flight1 = new Flight('Belgrade-Paris', new Date('2018-12-31'));
+    // var flight2 = new Flight('Barcelona-Belgrade', new Date('2019-01-07'));
+    // flight1.addPassenger(passenger1);
+    // flight1.addPassenger(passenger2);
     // console.log(flight1.listOfPassengers);
 
-    console.log(flight1.getData());
+    // console.log(flight1.getData());
+
+    // var person3 = createPassenger('visnja', 'Mrdelj', '', 'b');
+    // console.log(person3);
+    // var person4 = createPassenger('Mira', 'Grbic', '', 'e');
+    // console.log(person4);
+
 
 
 })();
---
+++ V WEEK/1_day/probavanje.js
@@ -0,0 +1,54 @@
+var a = 'BelgrAe';
+var naseSlovo;
+var rezultat;
+
+for (i = a.length - 1; i >= 0; i--) {
+    if (a[i].toLowerCase() != 'a' && a[i] != 'e' && a[i] != 'i' && a[i] != 'o' && a[i] != 'u') {
+        naseSlovo = a[i];
+        break;
+    }
+}
+
+rezultat = a[0] + naseSlovo.toUpperCase();
+console.log(rezultat);
+
+
+var str = 'Belgrade - New York';
+
+var words = str.split(' - ');
+console.log(words);
+// expected output: "fox"
+
+var chars = str.split('');
+console.log(chars[8]);
+// expected output: "k"
+
+var strCopy = str.split();
+console.log(strCopy);
+// expected output: Array ["The quick brown fox jumped over the lazy dog."]
+
+
+
+
+var relation = 'New York - Belgrade';
+newAbbForRelation = function () {
+
+    var splitDestination = relation.split(' - ');
+
+    function lastConsonant(string) {
+        var i;
+        var temp;
+        for (i = string.length - 1; i >= 0; i--) {
+
+            if (string[i].toLowerCase() != 'a' && string[i] != 'e' && string[i] != 'i' && string[i] != 'o' && string[i] != 'u') {
+                temp = string[i];
+                break;
+            }
+        }
+        return string[0] + temp.toUpperCase();
+    }
+
+    return lastConsonant(splitDestination[0]) + ' - ' + lastConsonant(splitDestination[1]);
+
+}
+console.log(newAbbForRelation());
